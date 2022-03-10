257377226030eed763a06fe63e925b4b3f980248
"feqafaith" <"ofegha@gmail.com">
2022-03-10 13:55:51 +0100

    hello

---
+++ contracts/HelloWord.sol
@@ -0,0 +1,17 @@
+//"SPDX-License-Identifier:MIT
+pragma solidity 0.8.7;
+contract HelloWorld{
+ string emone;
+ string name;
+
+ constructor (string memory employee)
+ {
+     employee = name;
+ }
+ function writeName(string memory) public {
+
+ }
+ function checkName() public view returns(string memory){
+return name;
+ }
+}
